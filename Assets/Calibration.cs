ede52977d5514c1b5fe7594526b0f2d179c4e957
benoitgeslainsegula <benoit.geslain@segula.fr>
2022-07-11 18:24:59 +0200

    Calibration terminée

--- Assets/Calibration.cs
+++ Assets/Calibration.cs
@@ -6,37 +6,85 @@ using Oculus.Interaction.Input;
 public class Calibration : MonoBehaviour
 {
     public Controller leftController; 
-    public GameObject Desk;
+    public GameObject Table;
+    public GameObject pivotPoint;
     public GameObject CameraRig;
 
     private Pose controllerPose;
-    private Vector3 sizeofDesk;
+    private Transform TDesk;
+    private Pose pointerPose;
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 sizeofDesk = Desk.GetComponentInChildren<BoxCollider>().size;
+        TDesk = Table.GetComponent<Transform>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("c")){
-            if (leftController.isActiveAndEnabled){
-                if (!leftController.TryGetPose(out controllerPose))
-                    return;
-
-            Debug.Log("CALIBRATION");
-
-            Vector3 offset = new Vector3(sizeofDesk.x/2, 0, -0.10f);
-            CameraRig.transform.position = controllerPose.position + offset;
-
-            Vector3 newRotation = leftController.transform.rotation.eulerAngles - CameraRig.transform.rotation.eulerAngles;
-            CameraRig.transform.rotation  = Quaternion.Euler(newRotation);
 
 
-            }
-            
+        if (Input.GetKeyDown("c"))
+        {
+            if (!leftController.TryGetPose(out controllerPose))
+                Debug.Log("Return");
+
+           
+            pivotPoint.GetComponent<Transform>().position = controllerPose.position;
+            pivotPoint.GetComponent<Transform>().rotation = Quaternion.Euler(0, controllerPose.rotation.eulerAngles.y, 0);
+            Debug.Log("Controller" + controllerPose.position);
+            Debug.Log("Pivot" + pivotPoint.transform.position);
         }
+
     }
+
+//    void OldCalib()
+//    {
+//        if (leftController.TryGetPose(out controllerPose))
+//            leftController.TryGetPointerPose(out pointerPose);
+//        TDesk.rotation = Quaternion.Euler(0, pointerPose.rotation.eulerAngles.y, 0);
+
+
+//        if (OVRInput.GetDown(OVRInput.Button.One))
+//        {
+//            Debug.Log("Entrée ici");
+//            if (leftController.isActiveAndEnabled)
+//            {
+
+
+//                if (!leftController.TryGetPose(out controllerPose))
+//                    return;
+//                if (!leftController.TryGetPointerPose(out pointerPose))
+//                    return;
+//                float teta = controllerPose.rotation.eulerAngles.y;
+//                TDesk.rotation = Quaternion.Euler(0.0f, teta, 0.0f);
+
+
+
+//                float c = Mathf.Cos(Mathf.Deg2Rad * teta);
+//                float s = Mathf.Sin(Mathf.Deg2Rad * teta);
+
+//                float x = TDesk.localScale.x / 2;
+//                float z = TDesk.localScale.z / 2;
+
+//                TDesk.position = controllerPose.position + new Vector3(c * x - s * z, 0, s * x + c * z);
+//                Debug.Log("Controller position" + controllerPose.position);
+//            }
+
+//        }
+//        if (Input.GetKeyDown("m"))
+//        {
+//            if (!leftController.TryGetPointerPose(out pointerPose))
+//                return;
+//            if (!leftController.TryGetPose(out controllerPose))
+//                return;
+
+//            //Debug.Log("Pointer rotation" + pointerPose.rotation.eulerAngles);
+//            Debug.Log("Pointer position" + pointerPose.position);
+//            //Debug.Log("controller position" + controllerPose.position);
+//        }
+
+    
+//}
 }
